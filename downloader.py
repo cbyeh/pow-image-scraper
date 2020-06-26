6d62bb399c4eb655a387bbb2f9fccd1200907f55
Chris <christopherbcyeh@gmail.com>
2020-06-25 22:23:05 -0700

    Fix non-quote url and add better valid url checker

--- downloader.py
+++ downloader.py
@@ -1,9 +1,11 @@
 # Quick script to scrape masked images from picturesofwalls.com without regular expressions
 # Dependencies: `pip install beautifulsoup4` `pip install requests`
+# Author: cbyeh
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
 from urllib.request import urlretrieve
+# src images in id main-img text contains invalids like ' ' and (1)
+from urllib.parse import quote
 
 base = 'http://picturesofwalls.com/'
 # As images are associated with a page and each image has a unique id
@@ -13,21 +15,21 @@ extension = 'gallery.asp?album=0&id={0}'
 
 # Check whether url can load
 def _is_valid(url):
-    parse = urlparse(url)
-    return bool(parse.netloc) and bool(parse.scheme)
+    request = requests.head(url)
+    return request.status_code == requests.codes.ok
 
 
 # As of June 2020, images before 142 are deprecated and latest is 16791
-for i in range(142, 16792):
+for i in range(1, 16792):
     url = base + extension.format(i)
     # Find image with id "main-image". We are only interested in the main photo in the Database
     response = requests.get(url)
     if _is_valid(url) and response.ok:
         soup = BeautifulSoup(response.content, 'html.parser')
-        img_extension = soup.find(id='main-image')['src']
+        img_extension = quote(soup.find(id='main-image')['src'])
         img_url = base + img_extension  # Url of image
         # Write file
-        if _is_valid(img_url) and (img_extension.endswith('.jpg') or img_extension.endswith('.jpeg') or img_extension.endswith('.png')):
+        if _is_valid(img_url):
             print('Downloading from id: ' + str(i) + ' at: ' + img_url)
             # Download with filename as id.{original filename}
             urlretrieve(img_url, 'out/' + str(i) + '.' + img_extension[19:])
